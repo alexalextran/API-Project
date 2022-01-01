ae7df5625c270e279ac2078df5f8b499399e8359
alexalextran <85623027+alexalextran@users.noreply.github.com>
2022-01-01 16:05:02 +1100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ main()
 
 function showUserPosts(id) {
     localStorage.setItem("id", id)
-    window.location.href = "https://alexalextran.github.io/user.html"
+    window.location.href = "https://alexalextran.github.io/API-Project/user.html"
     console.log(id)
 }
 
