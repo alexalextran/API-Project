6cb3e9e0097474c775815983f13fa02e85a58ee9
alexalextran <alextran8385@gmail.com>
2022-01-01 15:52:59 +1100

    bug fixing

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ main()
 
 function showUserPosts(id) {
     localStorage.setItem("id", id)
-    window.location.href = `http://user.html`
+    window.location.href = "user.html"
     console.log(id)
 }
 
