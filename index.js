15fdd615d0e0ca6c8ff7caefc8d7d972fae64caa
alexalextran <alextran8385@gmail.com>
2022-01-01 15:58:51 +1100

    .

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ main()
 
 function showUserPosts(id) {
     localStorage.setItem("id", id)
-    window.location.href = "user.html"
+    window.location.href = "https://alexalextran.github.io/user.html"
     console.log(id)
 }
 
